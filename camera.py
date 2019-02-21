6042a60dceafd12a63b341e86ab239b04520db01
Fabrice Depaulis <fabrice.depaulis@orange.com>
2019-02-21 18:34:19 +0100

    add things

--- camera.py
+++ camera.py
@@ -12,8 +12,8 @@ import os
         - Allume une led pour chaque prise de vue (3 ou 4 consécutives) + flash l'écran pour chaque prise de vue (1 seconde entre chacune des 4 prises)
         - Les photos sont enregistrées puis assemblées verticalement
         - La photo finale est postée sur le net
-    Bouton + led effet B&W
-    Bout on + led effet Vintage
+    Bouton mode : B&W -> B&W + Vintage -> Vintage -> Normal -> B&W -> ...
+    Bouton last : Affiche la dernière photo
 
     Bush button long : quitter l'appli
 '''
@@ -36,14 +36,20 @@ def shot(num_pic:int=3) -> list:
     picture_name = now.strftime('%d_%m_%H_%M_%S')
     pictures = ['{}_{}.jpeg'.format(picture_name, i) for i in range(num_pic)]
 
+    # capture_sequence does the job but doesn't simulate a flash
+    # camera.capture_sequence(pictures)
+
     for pic in pictures:
         flash() 
         camera.capture(pic)
         sleep(1)
 
-    combine(pictures)
+    shot = combine(picture_name, pictures)
+    loadpic(shot)
 
-def combine(files):
+def combine(base_name:str, files:list) -> str:
+    output_name = '{}.jpeg'.format(base_name)
+
     result = Image.new("RGB", (420, 790))
 
     for index, file in enumerate(files):
@@ -56,65 +62,38 @@ def combine(files):
         print('pos {0},{1} size {2},{3}'.format(x, y, w, h))
         result.paste(img, (x, y, x + w, y + h))
 
-    result.save(os.path.expanduser('image.jpeg'))
+    result.save(os.path.expanduser(output_name))
+    return output_name
 
+def loadpic(imageName): # affiche imagename
+    print("loading image: " + imageName)
+    screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+    background = pygame.image.load(imageName);
+    background.convert_alpha()
+    background = pygame.transform.scale(background,(width,height))
+    screen.blit(background,(0,0),(0,0,width,height))
+    pygame.display.flip()
+    sleep(2)
+    pygame.display.quit()
 
 camera = picamera.PiCamera()
 camera.rotation = 180
-
-# By default, the resolution is the display resolution.
 # camera.resolution = (2592, 1944)
-#camera.preview.fullscreen = True
-#camera.preview.window = (0, 0, 640, 480)
-##try:
-##    camera.start_preview()
-##    camera.annotate_text = "Hello world!"
-##    sleep(10)
-##    camera.stop_preview()
-##finally:
-##    camera.close()
-
-
-# Capturer une séquence
-# import time
-# import picamera
-# with picamera.PiCamera() as camera:
-#     camera.start_preview()
-#     time.sleep(2)
-#     camera.capture_sequence([
-#         'image1.jpg',
-#         'image2.jpg',
-#         'image3.jpg',
-#         'image4.jpg',
-#         ])
-#     camera.stop_preview()
+# camera.preview.fullscreen = True
+# camera.preview.window = (0, 0, 640, 480)
 
 camera.start_preview()
 
 while True:
-    if button.is_pressed:
+    try:
+        button.wait_for_press()
         camera.annotate_text = "3"
         sleep(1)
         camera.annotate_text = "2"
         sleep(1)
         camera.annotate_text = "1"
         sleep(1)
-        print("Pressed")
         shot()
+    except KeyboardInterrupt:
         camera.stop_preview()
-    else:
-        print("Released")
-    sleep(1)
-
-camera.stop_preview()
-
-# Combine 4 images in one
-# import os
-
-
-# files = [
-#   '~/Downloads/1.jpg',
-#   '~/Downloads/2.jpg',
-#   '~/Downloads/3.jpg',
-#   '~/Downloads/4.jpg']
-
+        break
