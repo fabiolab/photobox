172031d6d226381da676a446193edb7811df10ac
Fabrice Depaulis <fabypubs@yahoo.fr>
2019-02-22 09:49:18 +0100

    display combined image

--- camera.py
+++ camera.py
@@ -2,9 +2,9 @@ from gpiozero import Button
 from time import sleep
 from datetime import datetime
 import picamera
-import pygame
 from PIL import Image
 import os
+import sys
 
 '''
     Push button : 
@@ -18,11 +18,8 @@ import os
     Bush button long : quitter l'appli
 '''
 
-button = Button(18)
-
 def end():
     camera.close()
-    pygame.quit()
     sys.exit(0)
 
 def flash():
@@ -31,9 +28,17 @@ def flash():
     sleep(0.3)
     camera.brightness = 50
     
-def shot(num_pic:int=3) -> list:
+def shoot(num_pic:int=3) -> list:
+    camera.annotate_text = "3"
+    sleep(1)
+    camera.annotate_text = "2"
+    sleep(1)
+    camera.annotate_text = "1"
+    sleep(1)
+    camera.annotate_text = ""
+
     now = datetime.now()
-    picture_name = now.strftime('%d_%m_%H_%M_%S')
+    picture_name = now.strftime('%Y%m%d_%H%M%S')
     pictures = ['{}_{}.jpeg'.format(picture_name, i) for i in range(num_pic)]
 
     # capture_sequence does the job but doesn't simulate a flash
@@ -45,7 +50,6 @@ def shot(num_pic:int=3) -> list:
         sleep(1)
 
     shot = combine(picture_name, pictures)
-    loadpic(shot)
 
 def combine(base_name:str, files:list) -> str:
     output_name = '{}.jpeg'.format(base_name)
@@ -63,37 +67,52 @@ def combine(base_name:str, files:list) -> str:
         result.paste(img, (x, y, x + w, y + h))
 
     result.save(os.path.expanduser(output_name))
-    return output_name
+    o = display(output_name)
+    sleep(3)
+    camera.remove_overlay(o)
 
-def loadpic(imageName): # affiche imagename
-    print("loading image: " + imageName)
-    screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-    background = pygame.image.load(imageName);
-    background.convert_alpha()
-    background = pygame.transform.scale(background,(width,height))
-    screen.blit(background,(0,0),(0,0,width,height))
-    pygame.display.flip()
-    sleep(2)
-    pygame.display.quit()
+def display(image):
+    img = Image.open(image)
+
+    # Create the support image that will take all the screen
+    background_image = Image.new("RGB", camera.resolution)
+    
+    # The width must be a multiple of 32
+    # The height must be a multiple of 16
+    # cf add_overlay documentation
+    background_image = Image.new('RGB', (
+                        ((camera.resolution[0] + 31) // 32) * 32,
+                        ((camera.resolution[1] + 15) // 16) * 16,
+                        ))
+    
+    # Paste the original image into the padded one
+    # Center the image
+    center_x = camera.resolution[0]//2 - img.size[0]//2
+    center_y = camera.resolution[1]//2 - img.size[1]//2
+    
+    background_image.paste(img, (center_x, center_y))
+
+    # Add the overlay with the padded image as the source,
+    # but the original image's dimensions
+    o = camera.add_overlay(background_image.tobytes(),
+                           size=camera.resolution)
+    # By default, the overlay is in layer 0, beneath the
+    # preview (which defaults to layer 2). Here we make
+    # the new overlay semi-transparent, then move it above
+    # the preview
+    # o.alpha = 128
+    o.layer = 3
+    return o
 
 camera = picamera.PiCamera()
 camera.rotation = 180
-# camera.resolution = (2592, 1944)
-# camera.preview.fullscreen = True
-# camera.preview.window = (0, 0, 640, 480)
+camera.flash_mode = "auto"
+#camera.preview_fullscreen=False
+camera.annotate_text_size = 160
+camera.start_preview(resolution=(camera.resolution[0]//2, camera.resolution[1]//2))
+                     #fullscreen=False,
+                     #window = (300, 100, 640, 480))
 
-camera.start_preview()
-
-while True:
-    try:
-        button.wait_for_press()
-        camera.annotate_text = "3"
-        sleep(1)
-        camera.annotate_text = "2"
-        sleep(1)
-        camera.annotate_text = "1"
-        sleep(1)
-        shot()
-    except KeyboardInterrupt:
-        camera.stop_preview()
-        break
+button = Button(18)
+button.when_pressed = shoot
+button.when_held = end
