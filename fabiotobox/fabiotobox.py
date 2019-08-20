58c459b3922dcb8a9c4c5883a6f7838dddbe166e
Fabrice Depaulis <fabrice.depaulis@orange.com>
2019-08-20 17:53:05 +0200

    Add diaporama support

--- fabiotobox/fabiotobox.py
+++ fabiotobox/fabiotobox.py
@@ -4,11 +4,22 @@ from fabiotobox.camera import Camera
 from fabiotobox.photohandler import PhotoHandler
 from fabiotobox.tumblr import Tumblr
 from enum import IntEnum
+import pendulum
+import glob
+import random
+
+SCREENSAVER_DELAY = 5
+PHOTO_DIR = "/tmp"
 
 
 class PhotoFormat(IntEnum):
     PHOTOBOX = 0
     POLAROID = 1
+
+
+class Mode(IntEnum):
+    PHOTOBOX = 0
+    DIAPORAMA = 1
 
 
 class Fabiotobox:
@@ -32,19 +43,43 @@ class Fabiotobox:
         self.tumblr = tumblr
         self.photo_format = PhotoFormat.PHOTOBOX
         self.event_title = event_title
+        self.mode = Mode.PHOTOBOX
+        self.diaporama_countdown = pendulum.datetime()
+        self.photos = list()
 
     def run(self):
+        self.load_photos(PHOTO_DIR)
         self.shoot_button.when_held = self.camera.end
         self.camera.start_preview()
+        self.reset_diaporama_countdown()
 
         while True:
-            if self.shoot_button.is_pressed:
-                logger.debug("Button pressed")
-                photo = self.handle_button_pressed()
-                self.camera.display_image(photo)
+            if self.is_diaporama_countdown_reached():
+                self.mode = Mode.DIAPORAMA
+            else:
+                self.mode = Mode.PHOTOBOX
 
-                logger.info("Sending {} to tumblr".format(photo))
-                self.tumblr.post_photo(photo, self.event_title, [])
+            if self.mode is Mode.PHOTOBOX:
+                self.run_photobox()
+            else:
+                self.run_diaporama()
+
+    def run_photobox(self):
+        if self.shoot_button.is_pressed:
+            logger.debug("Button pressed for a photo")
+            photo = self.handle_button_pressed()
+            self.camera.display_image(photo)
+
+            logger.info("Sending {} to tumblr".format(photo))
+            self.tumblr.post_photo(photo, self.event_title, [])
+
+    def run_diaporama(self):
+        if self.shoot_button.is_pressed:
+            logger.debug("Button pressed for exiting diaporama")
+            self.mode = Mode.PHOTOBOX
+            self.reset_diaporama_countdown()
+        else:
+            self.camera.display_image(random.choice(self.photos))
 
     def handle_button_pressed(self) -> str:
         if self.photo_format == PhotoFormat.POLAROID:
@@ -55,3 +90,16 @@ class Fabiotobox:
             photo = self.photo_handler.combine(pictures)
 
         return photo
+
+    def reset_diaporama_countdown(self):
+        self.diaporama_countdown = pendulum.datetime().add(minutes=SCREENSAVER_DELAY)
+
+    def is_diaporama_countdown_reached(self) -> bool:
+        return self.diaporama_countdown > pendulum.datetime()
+
+    def load_photo(self, dir_path: str):
+        logger.debug(f"Loading photos from {dir_path}")
+        self.photos = [
+            filename for filename in glob.glob(f"{dir_path}/**/*", recursive=True)
+        ]
+        logger.debug(f"{len(self.photos)} photos loaded from {dir_path}")
