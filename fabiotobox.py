f1fe64c6c1a98cfd48af3ff5b03727481e6027f2
Fabrice Depaulis <fabypubs@yahoo.fr>
2019-08-27 18:54:23 +0200

    merge

--- fabiotobox.py
+++ fabiotobox.py
@@ -5,7 +5,7 @@ from fabiotobox.photohandler import PhotoHandler
 from fabiotobox.diaporama import Diaporama
 from fabiotobox.tumblr import Tumblr
 
-PHOTO_DIR = "/media/pi/2078B0CD25633F53/Backup/Photos/2016"
+PHOTO_DIR = "/media/pi/2078B0CD25633F53/Backup/Photos"
 
 
 def run():
@@ -13,7 +13,14 @@ def run():
     camera = Camera(storage_dir="photos/", rotate=0, fullscreen=True)
     diapo = Diaporama(PHOTO_DIR)
     photo_handler = PhotoHandler(storage_dir="photos/")
-    tumblr = Tumblr()
+    tumblr = Tumblr(
+        "N89rMJVwVBR0IrjZ8tRK3WJkGhIDQQT8Cr0zJ33sVVxSToOpno",
+        "TdKV5P0mCmGII2WFYUuQFe0xAwsvi1wrW7OhGZ9ydOKDSoOQpS",
+        "387VYuXTw8X3VigZiT1YAuGDIIbFcM43Ff4fMUlbkyKT2FT0dy",
+        "dErS8RPiyLUcWMkxhMsNWM2kLfA9KskYGo0gbEbl5Q1VykstFk",
+        # blog_name="manonetguillaume",
+        blog_name="fabiotobox",
+    )
     fabiotobox = Fabiotobox(
         camera=camera,
         diaporama=diapo,
